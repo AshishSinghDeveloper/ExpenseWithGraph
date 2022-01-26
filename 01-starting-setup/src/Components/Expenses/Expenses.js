333c7899aeb8ec42000ce81ba01798cba8e69744
AshishSinghDeveloper <ashishsng21@hotmail.com>
2022-01-26 13:46:25 -0600

    separates expense output conditions into one file.

--- 01-starting-setup/src/Components/Expenses/Expenses.js
+++ 01-starting-setup/src/Components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
-import ExpenseItem from "./ExpenseItem";
+import ExpenseList from "./ExpensesList";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -10,20 +10,9 @@ const Expenses = (props) => {
   const changeFilterHandler = (enteredyear) => {
     setFilteredYear(enteredyear);
   };
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-  let expenseContent = <p>No Expense Found.</p>;
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        date={expense.date}
-        title={expense.title}
-        amount={expense.amount}
-      />
-    ));
-  }
   return (
     <div>
       <Card className="expenses">
@@ -31,7 +20,7 @@ const Expenses = (props) => {
           selectedYear={filteredYear}
           onChangeFilter={changeFilterHandler}
         />
-        {expenseContent}
+        <ExpenseList items={filteredExpenses} />
       </Card>
     </div>
   );
